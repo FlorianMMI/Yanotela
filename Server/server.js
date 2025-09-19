e296fea5f4925792771e424849248a9d4674db85
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-09-19 08:36:09 +0200

    Fix, Save et création d'un dossier Server - Base de donnée mis en place

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,12 @@
 import { createServer } from 'http';
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
-
-
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 try {
     if (prisma) {
         console.log('Import Prisma OK');
+        
+        console.log('User created');
     }
 } catch (error) {
     console.error('Erreur import Prisma:', error);
@@ -15,11 +14,11 @@ try {
 
 const httpServer = createServer(async (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    
+
     res.end('Hello, World!');
 });
 
-const PORT = 300;
+const PORT = 3000;
 httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
