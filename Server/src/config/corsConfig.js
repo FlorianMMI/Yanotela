9dbb0c8730faac02dda8151cce0077b8efc4f7d3
Ethan <ethan.manchon@etu.unilim.fr>
2025-09-22 16:15:32 +0200

    US1.1 - patch cors

--- Server/src/config/corsConfig.js
+++ Server/src/config/corsConfig.js
@@ -5,6 +5,8 @@ const FRONT_URL = process.env.FRONT_URL || 'http://localhost:3000';
 const corsConfig = cors({
   origin: FRONT_URL,
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 });
 
 export default corsConfig;
