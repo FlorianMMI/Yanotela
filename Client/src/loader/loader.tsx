12e09f74d02ff266135566eaf9d5d1e848988db9
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-09-23 09:51:44 +0200

    US2.1 - Changement du loader

--- Client/src/loader/loader.tsx
+++ Client/src/loader/loader.tsx
@@ -16,7 +16,7 @@ export async function CreateNote(noteData?: Partial<Note>): Promise<Note | null>
             },
             body: JSON.stringify({
                 Titre: "Sans titre",
-                Content: "feur",
+                Content: "Sans Contenu",
                 authorId: 1,
             })
         });
