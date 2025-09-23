3b86f9816f9189d74e668f970ed43c25366f08d8
MaelValin <maelmy19@gmail.com>
2025-09-23 17:41:55 +0200

    US1.1- correction interraction front

--- Client/src/app/register/page.tsx
+++ Client/src/app/register/page.tsx
@@ -12,39 +12,39 @@ export default function Register() {
       <ReturnButton />
 
       <div className="flex flex-col justify-center items-center gap-6 mt-4">
-
-      <p className="
+        <p
+          className="
         text-center text-primary text-3xl font-bold md:min-w-[500px] after:content-[''] after:block after:w-full after:h-1 after:bg-primary after:rounded after:mt-8
-      ">
-        Bienvenue à bord 👋
-      </p>
+      "
+        >
+          Bienvenue à bord 👋
+        </p>
 
-      
+        <button
+          onClick={() => router.push("/register/form")}
+          className="w-full p-2.5 bg-primary rounded-lg flex items-center gap-3 hover:bg-primary-hover active:bg-primary-hover transition-colors cursor-pointer justify-center"
+        >
+          <Icon name="mail" className="text-white" size={20} />
+          <span className="text-white text-sm font-medium font-['Gantari']">
+            S&apos;inscrire avec un mail
+          </span>
+        </button>
 
-      <button
-        type="button"
-        className="w-full p-2.5 bg-white rounded-lg flex items-center gap-3 hover:bg-gray-200 active:bg-gray-50 transition-colors cursor-pointer justify-center"
-      >
-        <Icon name="google" className="text-primary" size={20} />
-        <span className="text-gray-700 text-sm font-medium font-['Gantari']">
-          S&apos;inscrire avec Google
-        </span>
-      </button>
+        <div className="flex items-center w-full gap-4">
+          <div className="flex-1 h-px bg-gray-300"></div>
+          <p className="text-gray-500 text-sm font-normal font-gant">ou</p>
+          <div className="flex-1 h-px bg-gray-300"></div>
+        </div>
 
-      <p className="text-2xl">
-        ou
-      </p>
-
-      <button
-        onClick={() => router.push('/register/form')}
-        className="w-full p-2.5 bg-primary rounded-lg flex items-center gap-3 hover:bg-primary-hover active:bg-primary-hover transition-colors cursor-pointer justify-center"
-      >
-        <Icon name="mail" className="text-white" size={20} />
-        <span className="text-white text-sm font-medium font-['Gantari']">
-          S&apos;inscrire avec un mail
-        </span>
-      </button>
-
+        <button
+          type="button"
+          className="w-full p-2.5 bg-white rounded-lg flex items-center gap-3 hover:bg-gray-200 active:bg-gray-50 transition-colors cursor-pointer justify-center"
+        >
+          <Icon name="google" className="text-primary" size={20} />
+          <span className="text-gray-700 text-sm font-medium font-['Gantari']">
+            S&apos;inscrire avec Google
+          </span>
+        </button>
       </div>
 
       <div className="flex flex-col justify-center items-center gap-2 mt-4">
@@ -52,7 +52,7 @@ export default function Register() {
           Vous avez déjà un compte ?
         </p>
         <button
-          onClick={() => router.push('/login')}
+          onClick={() => router.push("/login")}
           className="text-center justify-center text-primary hover:text-primary-hover active:text-primary-hover text-lg font-normal font-gant cursor-pointer"
         >
           Connectez-vous
