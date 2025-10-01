f0e80713ed80cc2edb68e4f2a8dd6a9472919df1
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-10-01 15:24:23 +0200

    US1.4 - Mise en route de la route update user dans le back avec en plus le loader

--- Server/src/routes/userRoutes.js
+++ Server/src/routes/userRoutes.js
@@ -7,5 +7,7 @@ const router = Router();
 // Route Get pour récupérer les informations de l'utilisateur (authentification requise)
 router.get('/info', requireAuth, userController.getUserInfo);
 
+router.post('/update', requireAuth, userController.updateUserInfo);
+
 export default router;
 
