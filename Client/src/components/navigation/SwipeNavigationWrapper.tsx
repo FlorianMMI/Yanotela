cdd3173068ddcd498f1f9a3af8e87804308bf68d
MaelValin <maelmy19@gmail.com>
2025-10-02 08:25:19 +0200

    us1.3-import react

--- Client/src/components/navigation/SwipeNavigationWrapper.tsx
+++ Client/src/components/navigation/SwipeNavigationWrapper.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import React from 'react'
 import { useSwipeNavigation } from '@/hooks/useSwipeNavigation';
 import { usePathname } from 'next/navigation';
 import { ReactNode } from 'react';
