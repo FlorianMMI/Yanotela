406a15c57c536c8b425eab71197e8c2995554ef2
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-10-07 15:41:45 +0200

    US4.2 - Fix et changement de mode de fonctionnement

--- Server/src/routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import NotificationController from '../controllers/NotificationController';
-
-
-
-const router = Router();
-
-router.get('/info', NotificationController.getInfo);
