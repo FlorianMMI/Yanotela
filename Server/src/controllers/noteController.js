90b5b63896616bd6aa9cfc24a8126f7c233e6afb
FlorianMMI <florian.bounissou@etu.unilim.fr>
2025-10-17 08:34:21 +0200

    Enhance note creation logic to initialize Content with default structure if empty

--- Server/src/controllers/noteController.js
+++ Server/src/controllers/noteController.js
@@ -97,12 +97,48 @@ export const noteController = {
         .json({ message: "Aucune donnée reçue dans req.body" });
     }
 
-    const { Titre, Content } = req.body;
+
+    let { Titre, Content } = req.body;
     // Récupérer l'authorId depuis la session au lieu du body
     const authorId = parseInt(req.session.userId); // Convertir en Int pour la DB
 
     if (!Titre) {
       return res.status(400).json({ message: "Titre requis" });
+    }
+
+    if (Content == "" || Content == null || Content == undefined) {
+      Content = JSON.stringify({
+        root: {
+          children: [
+            {
+              children: [
+                {
+                  detail: 0,
+                  format: 0,
+                  mode: "normal",
+                  style: "",
+                  text: "Commencez à écrire...",
+                  type: "text",
+                  version: 1
+                }
+              ],
+              direction: "ltr",
+              format: "",
+              indent: 0,
+              type: "paragraph",
+              version: 1,
+              textFormat: 0,
+              textStyle: ""
+            }
+          ],
+          direction: "ltr",
+          format: "",
+          indent: 0,
+          type: "root",
+          version: 1
+        }
+      });
+      
     }
 
     try {
