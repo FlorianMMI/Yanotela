e3320d78ad1b966ad92aa0baa578033c97ca2602
JulianDou <julian.doutreligne@gmail.com>
2025-12-08 09:04:06 +0100

    Maj apparence bouton Récupérer mes données

--- Client/src/ui/RGPDBouton.tsx
+++ Client/src/ui/RGPDBouton.tsx
@@ -1,27 +1,14 @@
 import { Setup2FA } from "@/loader/loader";
 
 export default function RGPDBouton() {
-
-
-        return (
-            <>
-
-                <div className="p-4 bg-white rounded shadow-md">
-                    
-                    <h2 className="text-xl font-bold mb-4">Recupérer mes données</h2>
-                    <button
-                        onClick={() => {
-                            Setup2FA();
-                        }}
-                        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors duration-200"
-                    >
-                        Récupérer mes données
-                    </button>
-                </div>
-            </>
-        )
-
-
-
-
+    return (
+        <button
+            onClick={() => {
+                Setup2FA();
+            }}
+            className="w-full px-4 py-2 border-2 border-primary text-primary rounded hover:bg-primary-hover hover:shadow-lg transition-all duration-300 cursor-pointer"
+        >
+            Récupérer mes données
+        </button>
+    )
 }
